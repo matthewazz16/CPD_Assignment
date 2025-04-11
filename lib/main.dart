b4e83639dc24620d41434ab001dd753c45091bc7
Matthew Azzopardi <matthewazzopardi05@gmail.com>
2025-04-11 08:36:40 +0200

    Task Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
-  runApp(QuickNotesApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize the local notifications plugin
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  const AndroidInitializationSettings initializationSettingsAndroid = 
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  final InitializationSettings initializationSettings = 
+    InitializationSettings(android: initializationSettingsAndroid);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  runApp(QuickNotesApp(flutterLocalNotificationsPlugin));
 }
 
 class QuickNotesApp extends StatelessWidget {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+  // Constructor to pass the plugin to HomeScreen
+  const QuickNotesApp(this.flutterLocalNotificationsPlugin, {Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +33,7 @@ class QuickNotesApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: HomeScreen(),
+      home: HomeScreen(flutterLocalNotificationsPlugin: flutterLocalNotificationsPlugin),
     );
   }
 }
